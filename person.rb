1d6cf000b8496cd915b7ffde165da65a41ad1010
Menanya <emmmiemenz@gmail.com>
2022-08-23 01:10:31 +0100

    ruby setup

---
+++ person.rb
@@ -0,0 +1,34 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(name = "Unknown", age, parent_permission: true)
+    @id = Random.rand(1..10000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  
+  def can_use_service?
+    if is_of_age? || @parent_permission
+      return true
+    end
+  end
+  
+  private
+  def is_of_age?
+    if @age >= 18
+      return true
+    else
+      return false
+    end
+  end
+
+
+
+end
+
+p1 = Person.new('morris', 27)
+p1.can_use_service?
+p p1
