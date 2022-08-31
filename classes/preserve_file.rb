bdd775405640ef635225ce49be0941e73df9e6ce
Menanya <emmmiemenz@gmail.com>
2022-08-31 23:44:10 +0100

    remove

--- classes/preserve_file.rb
+++ classes/preserve_file.rb
@@ -19,16 +19,4 @@ class PreserveFile
     puts "No file found to read on #{file}"
     nil
   end
-
-  # def read_json(options: {})
-  #   return [] unless File.exist?(@file_store)
-
-  #   file = File.new(@file_store, 'r')
-  #   file_data = JSON.parse(file.read, options)
-  #   file.close
-  #   file_data
-  # rescue StandardError
-  #   puts "No file found to read on #{@file_store}"
-  #   nil
-  # end
 end
